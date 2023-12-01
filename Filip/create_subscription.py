f3ff6c27578befdd3de7f130699bff3f9e8e6e51
Zehao <zehao.wu@eonerc.rwth-aachen.de>
2023-12-01 17:14:42 +0100

    make subscription

---
+++ Filip/create_subscription.py
@@ -0,0 +1,23 @@
+import json
+
+
+subscription = {
+    "description": "Subscription to receive MQTT-Notification about urn:ngsi-ld:Transaction:id",
+    "subject": {
+        "entities": [
+            {
+                "id": "urn:ngsi-ld:Transaction:id",
+                "type": "Transaction"
+            }
+        ]
+    },
+    "notification": {
+        "mqtt": {
+            "url": 'MQTT_Broker_URL',
+            "topic": "/v2/transactions/urn:ngsi-ld:Transaction:id/attrs"
+        }
+    }
+}
+file_path = "subscription.json"
+with open(file_path, 'w') as json_file:
+    json.dump(subscription, json_file, indent=2)
